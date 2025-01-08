eacd5ce9f6ebb38931e8a2b4f9e43df4cb35c3ed
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-01-07 23:43:55 -0600

    created food.js

---
+++ food.js
@@ -0,0 +1,86 @@
+[
+  {
+    "name": "Doro Wot",
+    "type": "Main Dish",
+    "ingredients": [
+      "Chicken",
+      "Onions",
+      "Berbere Spice",
+      "Niter Kibbeh",
+      "Boiled Eggs"
+    ],
+    "description": "A spicy chicken stew, considered Ethiopia's national dish.",
+    "servingSize": "4-6 people"
+  },
+  {
+    "name": "Injera",
+    "type": "Bread",
+    "ingredients": [
+      "Teff Flour",
+      "Water"
+    ],
+    "description": "A sourdough flatbread and staple of Ethiopian cuisine, used as a utensil to scoop up dishes.",
+    "servingSize": "Varies"
+  },
+  {
+    "name": "Kitfo",
+    "type": "Main Dish",
+    "ingredients": [
+      "Minced Beef",
+      "Niter Kibbeh",
+      "Mitrmitas Spice"
+    ],
+    "description": "Ethiopian steak tartare served with spiced butter and often accompanied by ayib (Ethiopian cheese).",
+    "servingSize": "2-4 people"
+  },
+  {
+    "name": "Shiro Wat",
+    "type": "Main Dish",
+    "ingredients": [
+      "Chickpea Flour",
+      "Berbere Spice",
+      "Onions",
+      "Garlic"
+    ],
+    "description": "A creamy, spicy stew made from ground chickpeas.",
+    "servingSize": "3-5 people"
+  },
+  {
+    "name": "Tibs",
+    "type": "Main Dish",
+    "ingredients": [
+      "Beef or Lamb",
+      "Onions",
+      "Tomatoes",
+      "Garlic",
+      "Spices"
+    ],
+    "description": "Sautéed meat dish, often served with injera.",
+    "servingSize": "2-4 people"
+  },
+  {
+    "name": "Firfir",
+    "type": "Breakfast Dish",
+    "ingredients": [
+      "Injera",
+      "Berbere Spice",
+      "Niter Kibbeh"
+    ],
+    "description": "Spicy, shredded injera dish often served for breakfast.",
+    "servingSize": "2-3 people"
+  },
+  {
+    "name": "Beyaynetu",
+    "type": "Vegetarian Platter",
+    "ingredients": [
+      "Lentils",
+      "Split Peas",
+      "Beets",
+      "Cabbage",
+      "Collard Greens",
+      "Injera"
+    ],
+    "description": "A colorful variety of vegetarian dishes served on a large injera.",
+    "servingSize": "4-6 people"
+  }
+]
